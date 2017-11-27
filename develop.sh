2e378ccf9f6f1dbd25f45b76a76deea88c2a9739
Matthias Loibl <mail@matthiasloibl.com>
2017-11-27 17:18:08 +0100

    Use Postgres 10

--- develop.sh
+++ develop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -e POSTGRES_PASSWORD=postgres -p 5432:5432 --name godep-postgres postgres:9.6-alpine
+docker run -d -e POSTGRES_PASSWORD=postgres -p 5432:5432 --name godep-postgres postgres:10
